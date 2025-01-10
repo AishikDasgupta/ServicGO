2c7988134053296f86f4a46cf911c2b7ad1eb522
Aishik Dasgupta <itsdg19@gmail.com>
2025-01-11 00:29:13 +0530

    `Update Hero component background image URL`

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,7 +14,7 @@ export function Hero() {
         className="absolute inset-0 bg-cover bg-center z-0"
         style={{
           backgroundImage:
-            'url("src/assets/3.png")',
+            'url("https://images.unsplash.com/photo-1736535454280-cce60b687886?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
         }}
       >
         <div className="absolute inset-0 bg-black/50" /> {/* Overlay */}
